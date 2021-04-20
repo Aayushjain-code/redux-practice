4f770bb27c9a53cdcfeab11c39ec74f75d3d3f01
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-04-21 00:01:57 +0530

    add to cart

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Home() {
+	return (
+		<div>
+			<div className="add-to-cart">
+				<img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" />
+			</div>
+			<h1>Home Component</h1>
+			<div className="cart-wrapper">
+				<div className="img-wrapper item">
+					<img src="https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBOPPO-F17-PROFUTU629745BED6286A/0..png?imwidth=320&impolicy=hq" />
+				</div>
+				<div className="text-wrapper item">
+					<span>
+						I-Phone
+                    </span>
+					<span>
+						Price: $1000.00
+                    </span>
+				</div>
+				<div className="btn-wrapper item">
+					<button>Add To Cart</button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Home
